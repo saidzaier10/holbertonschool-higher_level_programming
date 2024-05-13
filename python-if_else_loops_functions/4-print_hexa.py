4a475c48a5ec1e0b898027235b26939bdc0a2bcf
SaidZaier <8770@holbertonstudents.com>
2024-05-13 15:13:31 +0200

    chmod -R

--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 for nbr in range(0, 99):
     print("{:d} =".format(nbr), hex(nbr))
+    chmod +x /Users/said/Holbertonschool/holbertonschool-higher_level_programming/python-if_else_loops_functions/4-print_hexa.py
