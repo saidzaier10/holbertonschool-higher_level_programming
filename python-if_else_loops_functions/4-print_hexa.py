d374fc77c760e16316d58aa637c9c378648c2ad6
SaidZaier <8770@holbertonstudents.com>
2024-05-13 15:18:35 +0200

    change code task 4

--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-for nbr in range(0, 99):
-    print("{:d} =".format(nbr), hex(nbr))
-    chmod +x /Users/said/Holbertonschool/holbertonschool-higher_level_programming/python-if_else_loops_functions/4-print_hexa.py
+for i in range(99):
+    print("{} = 0x{:x}".format(i, i))
