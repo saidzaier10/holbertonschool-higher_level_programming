8fa732a0902c83152d9a626c507d92dc104f4b2c
SaidZaier <8770@holbertonstudents.com>
2024-05-13 15:22:43 +0200

    fix code task 6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
-for number in range(0, 90):
-    if number % 10 > number / 10:
-        if number != 89:
-            print("{:02d}, ".format(number), end="")
-        else:
-            print("{:02d}".format(number))
+for i in range(10):
+    for j in range(i + 1, 10):
+        if i != j:
+            print("{:d}{:d}".format(i, j), end="")
+            if i != 8 or j != 9:
+                print(", ", end="")
+print()
