fc895c3848f257f9dae29cb816de36ef8ec5a259
SaidZaier <8770@holbertonstudents.com>
2024-05-13 16:20:06 +0200

    add task 0,1,2,3,5 and README

---
+++ python-import_modules/5-variable_load.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    # Importer uniquement la variable `a` depuis `variable_load_5`
+    from variable_load_5 import a
+
+    # Afficher la valeur de `a`
+    print("{:d}".format(a))
