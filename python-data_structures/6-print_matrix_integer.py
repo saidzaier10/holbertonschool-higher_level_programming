94ef7f2ef0d8cbbec5e955c1a245831e71685c03
SaidZaier <8770@holbertonstudents.com>
2024-05-14 22:30:27 +0200

    function that print a matrix of intergers

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 
 
+# Define a function named print_matrix_integer that takes a matrix as input
 def print_matrix_integer(matrix=[[]]):
+    # Iterate over each row in the matrix
     for row in matrix:
-        for num in row:
-            print("{:d}".format(num), end=" ")
+        # Iterate over each element in the row
+        for i in range(len(row)):
+            # Check if the current element is not the last element in the row
+            if i != len(row) - 1:
+                # Print the current element with a space after it
+                print("{:d} ".format(row[i]), end="")
+            else:
+                # Print the last element without a space after it
+                print("{:d}".format(row[i]), end="")
         print()
-
-
-# Test de la fonction
-if __name__ == "__main__":
-    matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-    print_matrix_integer(matrix)
-    print("--")
-    print_matrix_integer()
