8d1c1f28e9ef4b41565f4780573ccab1b65633a7
SaidZaier <8770@holbertonstudents.com>
2024-05-14 16:08:25 +0200

    change last commit

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,18 +2,7 @@
 
 
 def new_in_list(my_list, idx, element):
-    """
-    Replace an element in a list at a specific index and return a new list.
 
-    Args:
-        my_list (list): The original list.
-        idx (int): The index of the element to be replaced.
-        element: The new element to be inserted at the specified index.
-
-    Returns:
-        list: A new list with the element replaced at the specified index.
-              If the index is negative or out of range, a copy of the original list is returned.
-    """
     # Check if the index is negative or out of range of the list
     if idx < 0 or idx >= len(my_list):
         return my_list[:]  # Return a copy of the original list
