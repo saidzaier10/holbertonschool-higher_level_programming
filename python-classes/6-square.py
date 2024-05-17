43571eac5ac6bcfa9b110eaadbcdb09091c86002
SaidZaier <8770@holbertonstudents.com>
2024-05-17 14:03:22 +0200

    class Square that defines a square by: (based on 5-square.py)

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -68,11 +68,16 @@ class Square:
                     """Return the position of the square"""
                     return self.__position
 
-                    @position.setter
-                    def position(self, value):
-                        """Check exception type and value for position"""
-                        if not isinstance(value, tuple) or len(value) != 2 or \
-                                not all(isinstance(num, int) for num in value) or \
-                                not all(num >= 0 for num in value):
-                            raise TypeError("position must be a tuple of 2 positive integers")
-                        self.__position = value
+                @position.setter
+                def position(self, value):
+                    """Check exception type and value for position"""
+                    if (
+                        not isinstance(value, tuple)
+                        or len(value) != 2
+                        or not all(isinstance(num, int) for num in value)
+                        or not all(num >= 0 for num in value)
+                    ):
+                        raise TypeError(
+                            "position must be a tuple of 2 positive integers"
+                        )
+                    self.__position = value
