33aa1ff82b989d1701739361954d5a7d3642f0d2
SaidZaier <8770@holbertonstudents.com>
2024-05-21 09:20:05 +0200

    function that raises a name exception with a message

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+"""
+This module contains a function that adds two integers
+"""
+
+
+def add_integer(a, b=98):
+    """
+    This function adds two integers
+    """
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
