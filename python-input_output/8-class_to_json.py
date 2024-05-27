739545212d3e6a4a44ba079a1fc5988b0eab582a
SaidZaier <8770@holbertonstudents.com>
2024-05-27 22:14:16 +0200

    function that returns the dictionary description with simple data structure (list, dictionary, string, integer and boolean) for JSON serialization of an object

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -7,3 +7,32 @@ import json
 def load_from_json_file(filename):
     with open(filename, "r", encoding="utf-8") as file:
         return json.load(file)
+
+
+def save_to_json_file(obj, filename):
+    with open(filename, "w", encoding="utf-8") as file:
+        return json.dump(obj, file)
+
+
+def class_to_json(obj):
+    return obj.__dict__
+
+
+def class_to_json_file(obj, filename):
+    save_to_json_file(class_to_json(obj), filename)
+
+
+def json_to_class(json_string):
+    return json.loads(json_string)
+
+
+def json_file_to_class(filename):
+    return json_to_class(load_from_json_file(filename))
+
+
+def json_to_class_file(json_string, filename):
+    class_to_json_file(json_to_class(json_string), filename)
+
+
+def json_file_to_class_file(json_filename, class_filename):
+    class_to_json_file(json_file_to_class(json_filename), class_filename)
