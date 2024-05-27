a69288ee2ef2fe35f9b128358eced582a042d1a9
SaidZaier <8770@holbertonstudents.com>
2024-05-27 22:26:46 +0200

    a class Student that defines a student by: (based on 9-student.py)

---
+++ python-input_output/10-student.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+Defines the Student class with public instance attributes and methods
+to retrieve a dictionary representation of a Student instance.
+"""
+
+
+class Student:
+    """
+    Represents a student with a first name, last name, and age.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a new Student instance.
+
+        Args:
+            first_name (str): The student's first name.
+            last_name (str): The student's last name.
+            age (int): The student's age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Returns a dictionary representation of the Student instance.
+
+        Args:
+            attrs (list, optional): List of attribute names to include
+            in the dictionary. If None, all attributes will be included.
+
+        Returns:
+            dict: A dictionary containing the specified attributes
+            of the student.
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            result = {}
+            for attr in attrs:
+                if hasattr(self, attr):
+                    result[attr] = getattr(self, attr)
+            return result
