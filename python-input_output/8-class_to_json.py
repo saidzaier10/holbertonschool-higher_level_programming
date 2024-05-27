467d2a2a8a49ba693139c9c80b249c460e74ee4f
SaidZaier <8770@holbertonstudents.com>
2024-05-27 22:15:30 +0200

    function that returns the dictionary description with simple data structure (list, dictionary, string, integer and boolean) for JSON serialization of an object

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,38 +1,20 @@
 #!/usr/bin/python3
-"""Convert a class to a JSON string."""
-
-import json
-
-
-def load_from_json_file(filename):
-    with open(filename, "r", encoding="utf-8") as file:
-        return json.load(file)
-
-
-def save_to_json_file(obj, filename):
-    with open(filename, "w", encoding="utf-8") as file:
-        return json.dump(obj, file)
+"""
+Defines a function that converts an object's attributes to a dictionary
+suitable for JSON serialization.
+"""
 
 
 def class_to_json(obj):
-    return obj.__dict__
+    """
+    Returns a dictionary representation of an object's attributes
+    for JSON serialization.
 
+    Args:
+        obj (object): An instance of a class.
 
-def class_to_json_file(obj, filename):
-    save_to_json_file(class_to_json(obj), filename)
-
-
-def json_to_class(json_string):
-    return json.loads(json_string)
-
-
-def json_file_to_class(filename):
-    return json_to_class(load_from_json_file(filename))
-
-
-def json_to_class_file(json_string, filename):
-    class_to_json_file(json_to_class(json_string), filename)
-
-
-def json_file_to_class_file(json_filename, class_filename):
-    class_to_json_file(json_file_to_class(json_filename), class_filename)
+    Returns:
+        dict: A dictionary containing the attributes of the object.
+    """
+    json_obj = vars(obj)
+    return json_obj
