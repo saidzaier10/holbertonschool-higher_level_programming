f544cc4391bf6492e43ccb7905edbaefdf0acea1
SaidZaier <8770@holbertonstudents.com>
2024-05-27 12:29:38 +0200

    add readme & function that reads a text file (UTF8) and prints it to stdout (0-read_file.py)

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+
+def read_file(filename=""):
+    """Read a file and print its content to stdout."""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
