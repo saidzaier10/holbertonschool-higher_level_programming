5a4a5f74bddcb0503d50e0b06ec67e4cccdd9b47
SaidZaier <8770@holbertonstudents.com>
2024-05-29 15:37:05 +0200

    add Readme & task_02_requests.py(a basic Python script to fetch posts from JSONPlaceholder using requests.get()

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Add all arguments to a Python list and save to a file."""
+
+
+import requests
+import csv
+
+
+def fetch_and_print_posts():
+    response = requests.get("https://jsonplaceholder.typicode.com/posts")
+    print(f"Status Code: {response.status_code}")
+
+    if response.status_code == 200:
+        posts = response.json()
+        for post in posts:
+            print(post['title'])
+
+
+def fetch_and_save_posts():
+    response = requests.get("https://jsonplaceholder.typicode.com/posts")
+    if response.status_code == 200:
+        posts = response.json()
+        posts_data = [{'id': post['id'], 'title': post['title'],
+                       'body': post['body']} for post in posts]
+
+        with open('posts.csv', 'w', newline='') as csvfile:
+            fieldnames = ['id', 'title', 'body']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            writer.writeheader()
+            for post in posts_data:
+                writer.writerow(post)
