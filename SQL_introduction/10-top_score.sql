f0197c06e3b7dd700ee6007b0537df5c16388d59
SaidZaier <8770@holbertonstudents.com>
2024-06-25 11:09:43 +0200

    add task 10 list all records

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,4 +1,2 @@
--- Order by score in descending order
-SELECT score, name
-FROM hbtn_0c_0.second_table
-ORDER BY score DESC;
+-- List all records from second_table ordered by score (top first)
+SELECT score, name FROM second_table ORDER BY score DESC;
