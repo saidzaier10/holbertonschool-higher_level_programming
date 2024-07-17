cb709b78e5136a44930bddcc04b98bc77a89926b
SaidZaier <8770@holbertonstudents.com>
2024-07-17 22:54:56 +0200

    Extending Dynamic Data Display to Include SQLite in Flask

--- python-server_side_rendering/task_04_db.py
+++ python-server_side_rendering/task_04_db.py
@@ -1,34 +1,18 @@
 from flask import Flask, render_template, request
 import json
 import csv
+import sqlite3
 
 app = Flask(__name__)
 
+
 def read_json_file(file_path):
-    """
-    Read data from a JSON file.
-
-    Args:
-        file_path (str): The path to the JSON file.
-
-    Returns:
-        list: The data read from the JSON file.
-    """
     with open(file_path, 'r') as file:
         data = json.load(file)
     return data
 
 
 def read_csv_file(file_path):
-    """
-    Read data from a CSV file.
-
-    Args:
-        file_path (str): The path to the CSV file.
-
-    Returns:
-        list: The data read from the CSV file.
-    """
     data = []
     with open(file_path, 'r') as file:
         reader = csv.DictReader(file)
@@ -37,14 +21,28 @@ def read_csv_file(file_path):
     return data
 
 
+def read_sqlite_db(db_path):
+    data = []
+    try:
+        conn = sqlite3.connect(db_path)
+        cursor = conn.cursor()
+        cursor.execute("SELECT id, name, category, price FROM Products")
+        rows = cursor.fetchall()
+        for row in rows:
+            data.append({
+                'id': row[0],
+                'name': row[1],
+                'category': row[2],
+                'price': row[3]
+            })
+        conn.close()
+    except sqlite3.Error as e:
+        print(f"Database error: {e}")
+    return data
+
+
 @app.route('/products')
 def products():
-    """
-    Display products based on the source and product ID.
-
-    Returns:
-        str: The rendered HTML template with the products and error message.
-    """
     source = request.args.get('source')
     product_id = request.args.get('id')
     data = []
@@ -54,6 +52,8 @@ def products():
         data = read_json_file('products.json')
     elif source == 'csv':
         data = read_csv_file('products.csv')
+    elif source == 'sql':
+        data = read_sqlite_db('products.db')
     else:
         error_message = "Wrong source"
 
